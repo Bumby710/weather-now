051e1fd0dbae46405c8bb441b8b4e6744a7e2baa
bumby710 <Georgeroman2001@gmail.com>
2019-11-04 15:16:51 -0500

    finished frount end development

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 */
 
 //middleweare
-app.use(express.static('.public'));
+app.use(express.static('./public'));
 
 app.get('/', function (req, res) {
   res.render('home.ejs');
